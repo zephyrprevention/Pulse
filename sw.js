e4ff98b225cab3fcc5c0727ef0de29c9fb6dc6a2
zephyrprevention <johndel577@gmail.com>
2026-08-11 10:34:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change SW_VERSION a chaque mise a jour importante (v1 -> v2 -> v3...)
-const SW_VERSION = 'pulse-v120';
+const SW_VERSION = 'pulse-v122';
 const ASSETS = ['./', './index.html', './manifest.json',
                 './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
